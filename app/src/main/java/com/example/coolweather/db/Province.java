feaa805dc8ce9cd416cf911c34a8b4ce9ab17b9b
Sunquan-sq <1034734443@qq.com>
2020-09-09 20:37:30 +0800

    加入创建数据库和表的各项配置

--- app/src/main/java/com/example/coolweather/db/Province.java
+++ app/src/main/java/com/example/coolweather/db/Province.java
@@ -3,4 +3,22 @@ package com.example.coolweather.db;
 import org.litepal.crud.DataSupport;
 
 public class Province extends DataSupport {
+
+    private int id;
+    private String provinceName;
+    private int getId(){
+        return  id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
